41b681c12fc4db1d49452e59c7a379b71daff128
dym3093 <youming3093@163.com>
2017-04-25 22:47:25 +0800

    修改spring-mvc.xml的文件名

---
+++ src/main/java/com/dayton/dolphin/threadCore/chap04/Point.java
@@ -0,0 +1,15 @@
+package com.dayton.dolphin.threadCore.chap04;
+
+/**
+ * Created by bruce on 17-4-25.
+ */
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
