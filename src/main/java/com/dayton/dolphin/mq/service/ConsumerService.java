6938444f2ab0fb6db1e96ce00f34e85bb9d77a74
dym3093 <youming3093@163.com>
2017-05-16 18:40:03 +0800

    添加消息队列服务类

---
+++ src/main/java/com/dayton/dolphin/mq/service/ConsumerService.java
@@ -0,0 +1,36 @@
+package com.dayton.dolphin.mq.service;/**
+ * Created by daimian on 17-5-16.
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+
+/**
+ * 消费者service
+ *
+ * @author Damian
+ * @create 2017-05-16 下午5:24
+ **/
+@Service
+public class ConsumerService {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    public TextMessage receive(Destination destination){
+        TextMessage textMessage = (TextMessage) jmsTemplate.receive();
+        try {
+            System.out.println("从队列 "+ destination.toString()+ " 收到了消息： \t "+textMessage.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        return textMessage;
+    }
+
+}
