b0feba6db57597839a9b6ce600b5b8978125305b
dym3093 <youming3093@163.com>
2017-05-22 18:18:43 +0800

    designMode: 添加Coffee抽象类

---
+++ src/main/java/com/dayton/dolphin/designModel/bridge/Coffee.java
@@ -0,0 +1,13 @@
+package com.dayton.dolphin.designModel.bridge;/**
+ * Created by daimian on 17-5-22.
+ */
+
+/**
+ * 咖啡
+ *
+ * @author Damian
+ * @create 2017-05-22 下午5:59
+ **/
+public abstract class Coffee {
+
+}
